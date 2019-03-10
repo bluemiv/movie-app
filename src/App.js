50716fcc702d4915940d5214a5e5c80421d78179
taehong kim <7538518@naver.com>
2019-03-10 22:03:06 +0900

    2019.03.10 loading state
    loading state

--- src/App.js
+++ src/App.js
@@ -24,26 +24,6 @@ class App extends Component {
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
   state = {
-    greeting: "Hello!",
-    // Map을 이용한 데이터 생성할 예정
-    movies: [
-      {
-        title: "사바하",
-        poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81616/81616_1000.jpg"
-      },
-      {
-        title: "증인",
-        poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81581/81581_185.jpg"
-      },
-      {
-        title: "극한직업",
-        poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81552/81552_1000.jpg"
-      },
-      {
-        title: "알리타",
-        poster: "https://cdn.clien.net/web/api/file/F01/7960316/c44f2d7231bf3.jpg"
-      },
-    ]
   }
 
   componentWillMount() {
@@ -55,25 +35,51 @@ class App extends Component {
     setTimeout(() => { // function은 오래된 방식, () => : 최신방식
       // console.log("Hello!")
       this.setState({
+        // Map을 이용한 데이터 생성할 예정
         movies: [
+          {
+            title: "사바하",
+            poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81616/81616_1000.jpg"
+          },
+          {
+            title: "증인",
+            poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81581/81581_185.jpg"
+          },
+          {
+            title: "극한직업",
+            poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81552/81552_1000.jpg"
+          },
+          {
+            title: "알리타",
+            poster: "https://cdn.clien.net/web/api/file/F01/7960316/c44f2d7231bf3.jpg"
+          },
           {
             title: "캡틴마블",
             poster: "https://file2.nocutnews.co.kr/newsroom/image/2019/03/06/20190306032809346893_0_777_1113.jpg"
           },
-          ...this.state.movies, // 이전 목록은 놔두고, 데이터 추가!
         ]
+        // ...this.state.movies, // 이전 목록은 놔두고, 데이터 추가!
       })
-    }, 5000)
+    }, 3000)
+  }
+
+  // 나의 기능과 리액트 자체 기능을 구분하기위해 언더스코어 사용하여 구분하면 좋음.
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => { // mapping!!
+        return <Movie title={movie.title} poster={movie.poster} key={index}/>  
+    })
+    return movies
   }
 
   render() { // 이 컴포넌트가 나한테 보여주려는 것이 무엇인가?
     console.log("Did render.")
     return (
       <div className="App">
+        {this.state.movies ? this._renderMovies() : "Loading..." /* loading state */}
         {/* {this.state.greeting} */}
-        {this.state.movies.map((movie, index) => { // mapping!!
+        {/* {this.state.movies.map((movie, index) => { // mapping!!
             return <Movie title={movie.title} poster={movie.poster} key={index}/>  
-        })}
+        })} */}
         
         {/* <Movie title={movieTitles[0]} poster={movieImages[0]}/>
         <Movie title={movieTitles[1]} poster={movieImages[1]}/>
