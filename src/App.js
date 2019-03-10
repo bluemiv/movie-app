caf7f4b322fc739e1428e42ad0d8de5f145d8064
taehong kim <7538518@naver.com>
2019-03-10 21:52:05 +0900

    2019.03.10 Life cycle
    Life cycle, state

--- src/App.js
+++ src/App.js
@@ -18,31 +18,60 @@ import Movie from "./Movie.js";
 //   "https://cdn.clien.net/web/api/file/F01/7960316/c44f2d7231bf3.jpg"
 // ]
 
-// Map을 이용한 데이터 생성할 예정
-const movies = [
-  {
-    title: "사바하",
-    poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81616/81616_1000.jpg"
-  },
-  {
-    title: "증인",
-    poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81581/81581_185.jpg"
-  },
-  {
-    title: "극한직업",
-    poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81552/81552_1000.jpg"
-  },
-  {
-    title: "알리타",
-    poster: "https://cdn.clien.net/web/api/file/F01/7960316/c44f2d7231bf3.jpg"
-  },
-]
-
 class App extends Component {
+  // Life cycle
+  // render: componentWillMount() -> render() -> componentDidMount()
+  // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
+  state = {
+    greeting: "Hello!",
+    // Map을 이용한 데이터 생성할 예정
+    movies: [
+      {
+        title: "사바하",
+        poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81616/81616_1000.jpg"
+      },
+      {
+        title: "증인",
+        poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81581/81581_185.jpg"
+      },
+      {
+        title: "극한직업",
+        poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81552/81552_1000.jpg"
+      },
+      {
+        title: "알리타",
+        poster: "https://cdn.clien.net/web/api/file/F01/7960316/c44f2d7231bf3.jpg"
+      },
+    ]
+  }
+
+  componentWillMount() {
+    // console.log("Life cycle 1: Will Mount.")
+  }
+  
+  componentDidMount() {
+    // console.log("Life cycle 3: Did Mount.")
+    setTimeout(() => { // function은 오래된 방식, () => : 최신방식
+      // console.log("Hello!")
+      this.setState({
+        movies: [
+          {
+            title: "캡틴마블",
+            poster: "https://file2.nocutnews.co.kr/newsroom/image/2019/03/06/20190306032809346893_0_777_1113.jpg"
+          },
+          ...this.state.movies, // 이전 목록은 놔두고, 데이터 추가!
+        ]
+      })
+    }, 5000)
+  }
+
   render() { // 이 컴포넌트가 나한테 보여주려는 것이 무엇인가?
+    console.log("Did render.")
     return (
       <div className="App">
-        {movies.map((movie, index) => { // mapping!!
+        {/* {this.state.greeting} */}
+        {this.state.movies.map((movie, index) => { // mapping!!
             return <Movie title={movie.title} poster={movie.poster} key={index}/>  
         })}
         
